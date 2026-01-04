6a7bf1b5a8b9818d5df151af02c04a794174fde4
George <george.arutinyan@gmail.com>
2026-01-04 22:58:57 +0400

    base url changed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lg-crm-backend.onrender.com/api",
   withCredentials: true,
 });
